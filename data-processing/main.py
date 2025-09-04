a1b9bc857e19dbed699d243d5d4261fd0692ca51
Sandevik <simon.sandevik@outlook.com>
2025-09-04 16:24:33 +0200

    data-processing setup

---
+++ data-processing/main.py
@@ -0,0 +1,59 @@
+
+from fastapi import FastAPI
+from time import time
+import psycopg2
+import glob
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL=os.getenv("DATABASE_URL")
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def run_migrations():
+    
+    conn = psycopg2.connect(DATABASE_URL)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    # Skapa tabell om inte migrationslogg finns
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS migrations (
+            id SERIAL PRIMARY KEY,
+            filename TEXT UNIQUE,
+            applied_at TIMESTAMP DEFAULT NOW()
+        );
+    """)
+
+    # Hitta alla SQL-filer
+    for file in sorted(glob.glob("migrations/*.sql")):
+        cur.execute("SELECT 1 FROM migrations WHERE filename = %s", (file,))
+        if cur.fetchone():
+            continue  # redan körd
+
+        with open(file, "r") as f:
+            sql = f.read()
+            cur.execute(sql)
+
+        cur.execute("INSERT INTO migrations (filename) VALUES (%s)", (file,))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+@app.get("/process/weather")
+async def route():
+    pass
+
+@app.get("/process/all")
+async def route():
+    pass
+
+@app.get("/process/aq")
+async def route():
+    pass
